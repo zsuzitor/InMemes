4af6853cbbc9083a60cb5ec6726968b62b26a813
zsuzitor <avdosevstas@mail.ru>
2017-08-26 16:54:02 +0300

    na4

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Im.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //если залогинен то на страницу
+            //если не залогинен то на главную
+            return View();
+        }
+
+        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Im.Startup))]
+namespace Im
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
